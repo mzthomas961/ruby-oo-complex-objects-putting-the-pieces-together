44ed078b761af1faf9ee5e2cf0250be3926f367b
Matthew Thomas <mzthomas96@gmail.com>
2021-01-12 21:54:42 -0500

    commit tree 8b3f6407865e7180dd21f24d1c0ddfcf05a12e49

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+        attr_accessor :brand,:size,:color,:material,:condition
+
+        def initialize (brand = "Adidas")
+            @brand =  brand
+        end
+        def cobble
+            @condition = "new"
+            puts "Your shoe is as good as new!"
+
+        end
+    end 
+
+
+    
